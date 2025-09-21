18449c1a4d0b78fcc055522d7fb188bf43a46856
Mohamed Moslemani <moslemanomohamed@gmail.com>
2025-09-21 22:16:09 +0300

    Delete BoundaeryValueProblem1D directory

--- BoundaeryValueProblem1D/tester.m
+++
@@ -1,6 +0,0 @@
-h = 0.25;
-[U,X,~] = Solve1D(h);
-X   = X(:);                        % make column
-uex = (1 - X).*sin(X);             % now column
-err = U - uex;                     % works
-disp([X U uex U-uex]);
